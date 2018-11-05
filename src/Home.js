dba76176de40fc4c1ac7f163fbc9567a8b050b8a
LiuJiangshan <ljs234@qq.com>
2018-11-05 23:51:43 +0800

    show image after picked

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Image, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import ImagePicker from 'react-native-image-picker'
 
-export default class Home extends Component {
+export default class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      imgSource: {uri: 'https://www.apple.com/v/imac-pro/c/images/overview/allinone_large.jpg'}
+    }
+  }
+
+  setImgSource (response) {
+    this.setState({imgSource: {uri: response.uri}})
+  }
+
   render () {
     const renderIcon = () => (<Image/>)
     const getLabel = () => 'label'
@@ -19,10 +30,12 @@ export default class Home extends Component {
     }
     const launchCamera = () => {
       ImagePicker.launchCamera(pcikerOptions, response => {
+        this.setImgSource(response)
       })
     }
     const openImageLibrary = () => {
       ImagePicker.launchImageLibrary(pcikerOptions, response => {
+        this.setImgSource(response)
       })
     }
 
@@ -30,6 +43,7 @@ export default class Home extends Component {
       <View>
         <Button title='启动相机' onPress={launchCamera}/>
         <Button title='打开相册' onPress={openImageLibrary}/>
+        <Image source={this.state.imgSource} style={{width: 500, height: 500}}/>
       </View>
     )
   }
